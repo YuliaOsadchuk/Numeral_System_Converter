8aeff7b9a6f36196c3905f9175785f71d548a27b
Yulia Osadchuk <yulia.osadchuk@gmail.com>
2021-01-12 17:10:45 +0200

    add problem Slightly mocking

---
+++ Problems/Slightly mocking/src/Main.java
@@ -0,0 +1,15 @@
+/*class Calculator {
+    private CalculatorEngine engine;
+
+    public Calculator(CalculatorEngine engine) {
+        this.engine = engine;
+    }
+
+    public String divide(int a, int b) {
+        try {
+            return "Division of " + a + " by " + b + " = " + engine.divide(a, b);
+        } catch (Exception e) {
+            return "Division by zero is prohibited";
+        }
+    }
+}*/
