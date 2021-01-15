63cd143f03dfd1ee4bb9f9ee7bcc431443178f46
Yulia Osadchuk <yulia.osadchuk@gmail.com>
2021-01-15 13:19:27 +0200

    add Stage 3/6: Convert decimals

--- Numeral System Converter/task/src/converter/Main.java
+++ Numeral System Converter/task/src/converter/Main.java
@@ -6,7 +6,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int octopus = Integer.parseInt(Integer.toOctalString(n));
-        System.out.println(octopus % 10);
+        int radix = scanner.nextInt();
+        if (radix == 16) {
+            System.out.println("0x" + Integer.toHexString(n));
+        } else if (radix == 8) {
+            System.out.println("0" + Integer.toOctalString(n));
+        } else if (radix == 2) {
+            System.out.println("0b" + Integer.toBinaryString(n));
+        }
+
     }
 }
