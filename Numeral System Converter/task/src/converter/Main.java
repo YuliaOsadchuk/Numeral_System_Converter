49d2711618408ff0cdcb81184ae84e4dfc448c89
Yulia Osadchuk <yulia.osadchuk@gmail.com>
2021-01-27 19:07:31 +0200

    start Stage 5/6: Converting fractions

--- Numeral System Converter/task/src/converter/Main.java
+++ Numeral System Converter/task/src/converter/Main.java
@@ -6,40 +6,42 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int sourceRadix = scanner.nextInt();
-        int number = scanner.nextInt();
+        String number = scanner.next();
         int targetRadix = scanner.nextInt();
-        String s = "";
-        if (sourceRadix == 1) {
-            int numberDex = Integer.parseInt(String.valueOf(String.valueOf(number).length()));
-            s = Integer.toString(numberDex, targetRadix);
-        } else if (sourceRadix == 10 && targetRadix != 1) {
-            s = Integer.toString(number, targetRadix);
-        } else if (sourceRadix ==10 && targetRadix == 1) {
-            if (sourceRadix == 10) {
-                for (int i = 0; i < number; i++) {
-                    s = s + 1;
-                }
-            } else {
-                int newNumber = Integer.parseInt(String.valueOf(number), sourceRadix);
-                for (int i = 0; i < newNumber; i++) {
-                    s = s + 1;
-                }
-            }
-        } else {
-            int newNumber = Integer.parseInt(String.valueOf(number), sourceRadix);
-            s = Integer.toString(newNumber, targetRadix);
-        }
-
-        System.out.println(s);
+        String[] n = number.split(".");
 
 
-        /*if (radix == 16) {
-            System.out.println("0x" + Integer.toHexString(n));
-        } else if (radix == 8) {
-            System.out.println("0" + Integer.toOctalString(n));
-        } else if (radix == 2) {
-            System.out.println("0b" + Integer.toBinaryString(n));
-        }*/
-
+        int numberDecimal = conversionToDecimal(sourceRadix, n[0]);
+        System.out.println(conversionToTargetRadix(numberDecimal, targetRadix));
     }
+
+    public static int conversionToDecimal(int sourceRadix, String number) {
+        number = conversionLetter(number);
+        if (sourceRadix == 1) {
+            return Integer.parseInt(String.valueOf(number.length()));
+        } else {
+            return Integer.parseInt(String.valueOf(number), sourceRadix);
+        }
+    }
+
+    public static String conversionToTargetRadix(int number, int targetRadix) {
+        if (targetRadix == 1) {
+            String s = "";
+            for (int i = 0; i < number; i++) {
+                s = s + 1;
+            }
+            return s;
+        } else {
+            return Integer.toString(number, targetRadix);
+        }
+    }
+
+    public static String conversionLetter(String number) {
+        char[] ch = number.toCharArray();
+        for (int i = 0; i < ch.length; i++) {
+
+        }
+        return number;
+    }
+
 }
